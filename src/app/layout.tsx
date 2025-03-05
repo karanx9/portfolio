7594fcaac563669f82a5b2d9aa31b91a2734072f
karan soni <bussinesskaran6@gmail.com>
2025-03-05 18:11:41 +0530

    New push1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { twMerge } from "tailwind-merge";
 import { Footer } from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({
   subsets: ["latin"],
